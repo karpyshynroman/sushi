a70566ad12a3943008d7c201e11425116bed84d4
Karpyshyn Roman <karpyshyn.roman@gmail.com>
2019-11-27 14:47:22 +0200

    Updates

---
+++ precache-manifest.15318e734f9db55b017084856e796470.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67c8e89f68d91a7f1cf6716001604baa",
+    "url": "/sushi/index.html"
+  },
+  {
+    "revision": "b1e2558ccfaf1333262e",
+    "url": "/sushi/static/css/main.61739d88.chunk.css"
+  },
+  {
+    "revision": "baaa74db0e9a40a3b9d9",
+    "url": "/sushi/static/js/2.0ae7695c.chunk.js"
+  },
+  {
+    "revision": "b1e2558ccfaf1333262e",
+    "url": "/sushi/static/js/main.4808ef27.chunk.js"
+  },
+  {
+    "revision": "ace1d349f34a59437d3c",
+    "url": "/sushi/static/js/runtime-main.f045fca6.js"
+  }
+]);
